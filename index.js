f146c54d061769fe7d521db9236e8387a7cdc5cb
TheRealKevBot <Kglas001@gmail.com>
2021-06-10 14:18:36 -0600

    First

---
+++ index.js
@@ -0,0 +1,15 @@
+// The new "Avengers" movie has just been released! 
+// There are a lot of people at the cinema box office standing in a huge line. 
+// Each of them has a single 100, 50 or 25 dollar bill. 
+// An "Avengers" ticket costs 25 dollars. 
+
+// Can Vasya sell a ticket to every person and give change, 
+// if he initially has no money and sells the tickets strictly in the order people queue?
+
+function tickets(peopleInLine){
+    // ...
+  }
+
+console.log(tickets([25, 25, 50, 50]), "Answer: YES");
+console.log(tickets([25, 100]), "Answer: NO");
+console.log(tickets([25, 25, 50, 50, 100]), "Answer: NO");
